05384a35f8e0f5d6667ffa5831dc7b826018aa61
MDTrue <MDTrueII@hotmail.com>
2018-12-10 03:01:11 -0800

    very rough draft of nodejs hw

---
+++ liri.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+var fs = require("fs");
+var request = require("request");
+var inquirer = require("inquirer")
+// var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+
+// console.log(spotify)
+start()
+function start() {
+  console.log("Good morning, what can I help you with today?\n");
+  mainMenu();
+}
+function mainMenu() {
+
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "menuChoice",
+          message: "Please pick one of my preprogrammed functions",
+          choices: ["concert-this", "spotify-this-song", "movie-this","do-what-it-says"]
+        }
+]).then(function(userChoice){
+    switch (userChoice.menuChoice){
+        case "concert-this":
+        runConcert();
+        break;
+
+        case "spotify-this-song":
+        runSpotify();
+        break;
+
+        case "movie-this":
+        runMovie();
+        break;
+
+        case "do-what-it-says":
+        runDoWhat();
+        break;
+    }
+})
+}
+function runConcert(){
+
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "artist",
+          message: "Please enter the band or artist of your choice",
+        }
+    ]).then(function(userInput){
+        var band = (userInput.artist);
+        // console.log(band)
+        var queryURL = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp"
+        request(queryURL,function(error, response, body){
+            // console.log(response)
+            if (!error) {
+                var event = JSON.parse(body);
+                // console.log(event)
+                for (var j = 0;j < event.length;j++){
+                    console.log(event[j].venue.name)
+                    console.log(event[j].venue.city,event[j].venue.region)
+                    console.log(event[j].datetime)
+                //    var  date = moment(day).format(MM-DD-YYYY)
+                    // console.log
+                }
+            }else {
+                console.log(error)
+              }
+        })
+    })
+}
+function runSpotify(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "song",
+          message: "Please enter the song of your choice",
+        }
+    ]).then(function(userInput){
+        var song = userInput.song;
+        console.log(song)
+    })
+}
+function runMovie(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "movie",
+          message: "Please enter the movie of your choice",
+        }
+    ]).then(function(userInput){
+        var movie =(userInput.movie)
+        console.log(movie)
+        var queryURL = "http://www.omdbapi.com/?apikey=90ca2cdc&="+movie;
+        request(queryURL,function(error,response,body){
+            console.log(response)
+            if (!error ) {
+
+                var movieInfo = JSON.parse(body);
+                console.log(movieInfo)
+            }
+        })
+    })
+
+}
